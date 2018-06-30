04e801a0deee823c6923727445d451e7b37e929c
foray1010 <foray1010@users.noreply.github.com>
2018-06-30 23:36:45 +0800

    test(cis194): add edge case tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,16 +5,19 @@ import Homework1
 main :: IO ()
 main = hspec $ describe "Homework1" $ do
   it "toDigits" $ do
-   toDigits 1234 `shouldBe` [1,2,3,4]
-   toDigits 0 `shouldBe` []
-   toDigits (-17) `shouldBe` []
+    toDigits 1234 `shouldBe` [1,2,3,4]
+    toDigits 0 `shouldBe` []
+    toDigits (-17) `shouldBe` []
 
-  it "toDigitsRev" $
+  it "toDigitsRev" $ do
     toDigitsRev 1234 `shouldBe` [4,3,2,1]
+    toDigitsRev 0 `shouldBe` []
+    toDigitsRev (-17) `shouldBe` []
 
   it "doubleEveryOther" $ do
     doubleEveryOther [8,7,6,5] `shouldBe` [16,7,12,5]
     doubleEveryOther [1,2,3] `shouldBe` [1,4,3]
+    doubleEveryOther [] `shouldBe` []
 
   it "sumDigits" $ do
     sumDigits [16,7,12,5] `shouldBe` 22
@@ -23,3 +26,4 @@ main = hspec $ describe "Homework1" $ do
   it "validate" $ do
     validate 4012888888881881 `shouldBe` True
     validate 4012888888881882 `shouldBe` False
+    validate 0 `shouldBe` True
