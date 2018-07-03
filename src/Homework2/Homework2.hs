c8c75e08f3360a872e0b71ef379e171011a28a5d
foray1010 <foray1010@users.noreply.github.com>
2018-07-03 22:52:32 +0800

    feat(cis194): finish homework2 exercise2-5

--- src/Homework2/Homework2.hs
+++ src/Homework2/Homework2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Homework2.Homework2 where
 
 import Homework2.Log
@@ -15,15 +17,37 @@ parse = map parseMessage . lines
 -- ex1 end
 
 -- ex2 start
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert log Leaf = Node Leaf log Leaf
+insert
+  log1@(LogMessage _ ts1 _)
+  tree@(Node left log2@(LogMessage _ ts2 _) right)
+  | ts1 > ts2 = Node left log2 (insert log1 right)
+  | otherwise = Node (insert log1 left) log2 right
 -- ex2 end
 
 -- ex3 start
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
 -- ex3 end
 
 -- ex4 start
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left log right) = inOrder left ++ [log] ++ inOrder right
 -- ex4 end
 
 -- ex5 start
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong =
+  map (\(LogMessage _ _ msg) -> msg) .
+  inOrder .
+  build .
+  filter (\case
+    LogMessage (Error level) _ _ -> level >= 50
+    _ -> False
+  )
 -- ex5 end
 
 -- ex6 start
--- test/Homework2Spec.hs
+++ test/Homework2Spec.hs
@@ -28,3 +28,60 @@ homework2Spec = describe "Homework2" $ do
       LogMessage Info 29 "la la la",
       LogMessage Warning 0 "la la la",
       Unknown "This is not in the right format"]
+
+  it "ex2: insert" $ do
+    insert (Unknown "1") Leaf `shouldBe` Leaf
+
+    insert (LogMessage Info 1 "1") Leaf `shouldBe` Node Leaf (LogMessage Info 1 "1") Leaf
+
+    insert
+      (LogMessage Info 2 "2")
+      (Node Leaf (LogMessage Info 1 "1") (Node Leaf (LogMessage Info 3 "3") Leaf))
+      `shouldBe`
+      Node Leaf (LogMessage Info 1 "1") (
+        Node (Node Leaf (LogMessage Info 2 "2") Leaf) (LogMessage Info 3 "3") Leaf
+      )
+
+  it "ex3: build" $ do
+    build [] `shouldBe` Leaf
+
+    build [
+      LogMessage Info 1 "1",
+      LogMessage Info 2 "2",
+      LogMessage Info 3 "3"]
+      `shouldBe`
+      Node (
+        Node (Node Leaf (LogMessage Info 1 "1") Leaf) (LogMessage Info 2 "2") Leaf
+      ) (LogMessage Info 3 "3") Leaf
+
+  it "ex4: inOrder" $ do
+    inOrder Leaf `shouldBe` []
+
+    inOrder (
+      Node (
+        Node (Node Leaf (LogMessage Info 1 "1") Leaf) (LogMessage Info 2 "2") Leaf
+      ) (LogMessage Info 3 "3") Leaf)
+      `shouldBe`
+      [
+        LogMessage Info 1 "1",
+        LogMessage Info 2 "2",
+        LogMessage Info 3 "3"]
+
+  it "ex5: whatWentWrong" $ do
+    whatWentWrong [] `shouldBe` []
+
+    whatWentWrong [
+      LogMessage Info 6 "Completed armadillo processing",
+      LogMessage Info 1 "Nothing to report",
+      LogMessage (Error 99) 10 "Flange failed!",
+      LogMessage Info 4 "Everything normal",
+      LogMessage Info 11 "Initiating self-destruct sequence",
+      LogMessage (Error 70) 3 "Way too many pickles",
+      LogMessage (Error 65) 8 "Bad pickle-flange interaction detected",
+      LogMessage Warning 5 "Flange is due for a check-up",
+      LogMessage Info 7 "Out for lunch, back in two time steps",
+      LogMessage (Error 20) 2 "Too many pickles",
+      LogMessage Info 9 "Back from lunch"] `shouldBe` [
+      "Way too many pickles",
+      "Bad pickle-flange interaction detected",
+      "Flange failed!"]
